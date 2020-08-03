a5a67f377e2fb8939f44d2f1869615365bd003a6
Lola <lola.donval@gmail.com>
2020-08-03 13:19:47 +0200

    create route to  get all projects

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,4 +4,12 @@ const connection = require('../helper/db')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  const sql = 'SELECT * FROM project'
+  connection.query(sql, (err, projects) => {
+    if (err) throw err
+    res.status(200).send(projects)
+  })
+})
+
 module.exports = router
